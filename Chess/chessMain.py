9b2da997a5cdf7f6270f851e881d5855b35be9d7
Manvir Mann <manvirm29@gmail.com>
2022-10-19 23:25:42 -0700

    Implement player click

--- Chess/chessMain.py
+++ Chess/chessMain.py
@@ -36,11 +36,25 @@ def main():
     gs = chessEngine.GameState()
     load_images() #only do this once, before while loop
     running = True
-
+    sqSelected = () #no square is selected, keep track of last click of user (tuple: (row, col))
+    playerClicks = [] #keep track of player clicks (two tuples: [(6, 4), (4, 4)]
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos() #(x, y) location of mouse
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col): #user clicked the same square twice
+                    sqSelected = () #unselect
+                    playerClicks = [] #clear player clicks
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected) #append for both 1st and 2nd clicks
+                if len(playerClicks) == 2: #after second click
+                    pass
+
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
