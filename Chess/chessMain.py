18fe74d1b6bc87b2baf538934afd1aab5b76055c
Manvir Mann <manvirm29@gmail.com>
2022-11-19 23:26:50 -0800

    fix double click on piece bug

--- Chess/chessMain.py
+++ Chess/chessMain.py
@@ -62,8 +62,10 @@ def main():
                     if move in validMoves:
                         gs.makeMove(move)
                         moveMade = True
-                    sqSelected = () #reset user clicks
-                    playerClicks = []
+                        sqSelected = () #reset user clicks
+                        playerClicks = []
+                    else:
+                        playerClicks = [sqSelected]
             #Key handler
             elif e.type == p.KEYDOWN:
                 if e.key == p.K_z: #undo when z is pressed
